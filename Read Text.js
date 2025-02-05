4ee87f0208392a5896627a87eb40029d7a7be314
Denis Hristov <denis.hristov145@gmail.com>
2025-02-05 20:54:33 +0200

    Add files via upload

---
+++ Read Text.js
@@ -0,0 +1,16 @@
+function writeThis(n) {
+    let i = 0;
+    while(n[i]!="Stop"){
+    console.log(n[i]);
+    i++;
+    } 
+}
+writeThis(["Nakov",
+    "SoftUni",
+    "Sofia",
+    "Bulgaria",
+    "SomeText",
+    "Stop",
+    "AfterStop",
+    "Europe",
+    "HelloWorld"]);
